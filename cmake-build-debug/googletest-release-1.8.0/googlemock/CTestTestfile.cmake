f8b974d371ee0755a56c5c4c758ca138f49119b6
LioraZ <liorazr@gmail.com>
2017-11-26 15:23:41 +0200

    removed cmake build debug

--- cmake-build-debug/googletest-release-1.8.0/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/liora/CLionProjects/othello/googletest-release-1.8.0/googlemock
-# Build directory: /home/liora/CLionProjects/othello/cmake-build-debug/googletest-release-1.8.0/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
