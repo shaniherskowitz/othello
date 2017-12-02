6c6a5b7241ae9e39826dde24ce4f4e51d83e9c6d
LioraZ <liorazr@gmail.com>
2017-12-02 19:13:03 +0200

    created server class

---
+++ Server.h
@@ -0,0 +1,27 @@
+//
+// Created by liora on 02/12/17.
+//
+
+#ifndef OTHELLO_SERVER_H
+#define OTHELLO_SERVER_H
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <string.h>
+#include <iostream>
+#include <stdio.h>
+
+class Server {
+    int port;
+    int serverSocket; // the socket's file descriptor
+    void handleClient(int clientSocket);
+    int calc(int arg1, const char op, int arg2) const;
+
+public:
+    Server(int port);
+    void start();
+    void stop();
+};
+
+
+#endif //OTHELLO_SERVER_H
