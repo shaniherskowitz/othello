bac98be7aa59b0d9068b51d947bca42b3bb4bbf1
LioraZ <liorazr@gmail.com>
2017-12-10 23:39:17 +0200

    changed filing so can be handed in

---
+++ client/Point.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cstring>
+#include <cstdio>
+#include "Point.h"
+
+Point::Point(int xVal, int yVal) {
+  x = xVal;
+  y = yVal;
+}
+
+Point::Point() { 
+  x = -1;
+  y = -1;
+}
+
+Point::~Point() {}
+
+const int Point::getX() const { return this->x; }
+
+const int Point::getY() const { return this->y; }
+
+ostream &operator<<(ostream &os, Point const &point) {
+  return cout << "(" << point.x << "," << point.y << ") ";
+}
+
+bool Point::operator==(const Point &point) const {
+  return (point.x == this->x && point.y == this->y);
+}
+
+bool Point::operator!=(const Point &point) const {
+  return !(point.x == this->x && point.y == this->y);
+}
+
+bool Point::operator<=(const Point &point) const {
+  return !(point.x <= this->x || point.y <= this->y);
+}
